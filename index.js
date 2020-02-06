c8d6cd736bcf6bd923411f2b17b3a47078dab6e1
Frank Valdez <fvaldez421@gmail.com>
2020-02-05 19:50:18 -0800

    added socket.io base code

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-import app from './config/server';
+import socketIo from 'socket.io';
+import server from './config/server';
+// database lines below will be implemented when set up
 // import './config/mongoose';
 
+
 const PORT = process.env.port || 8080;
+const io = socketIo(server);
+
+io.on('connection', socket => {
+  console.log('Client connected...');
+  socket.on('join', function(data) {
+    console.log(data);
+  });
+  socket.on('disconnect', () => {
+    console.log('Client disconnected.')
+  })
+})
 
 // Log any server errors
-app.on('error', err => {
+server.on('error', err => {
   console.error('Server error:', err);
 });
 
+
 // open port
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🌎 ===> Server listening on port ${PORT}!`);
 });
 
