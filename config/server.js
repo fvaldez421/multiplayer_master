bb635106d9e6e736423f4125d22d739d7ec2cd45
Frank Valdez <fvaldez421@gmail.com>
2020-02-05 19:49:46 -0800

    cleaned up express server code

--- config/server.js
+++ config/server.js
@@ -1,18 +1,33 @@
+import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import path from 'path';
 import routes from '../routes';
 
+
 const app = express();
+const server = http.createServer(app);
 
 // encode and parse our request data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 // handle logging
 app.use(morgan('combined'));
+
+/**
+ * The two lines below are soley used for testing socket.io while serving 
+ * static files from `views`. They will likely be removed after v1 of the 
+ * client side React app is up and has support for websockets
+ */
+app.use(express.static(__dirname + '/../node_modules'));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // implement our routes
 app.use('/', routes);
 
-export default app;
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../views/index.html'))
+})
+
+export default server;
