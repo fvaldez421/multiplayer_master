5dbcae7d30d784b176122a52a10d515adada1292
Frank Valdez <fvaldez421@gmail.com>
2020-02-08 21:31:18 -0800

    updated comments for test routes

--- config/server.js
+++ config/server.js
@@ -16,18 +16,19 @@ app.use(bodyParser.json());
 app.use(morgan('combined'));
 
 /**
- * The two lines below are soley used for testing socket.io while serving 
+ * The lines 23-27 below are soley used for testing socket.io while serving 
  * static files from `views`. They will likely be removed after v1 of the 
  * client side React app is up and has support for websockets
  */
 app.use(express.static(__dirname + '/../node_modules'));
 app.use(express.static(path.join(__dirname, 'views')));
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../views/index.html'))
+})
+
 
 // implement our routes
 app.use('/', routes);
 
-app.get('/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../views/index.html'))
-})
 
 export default server;
